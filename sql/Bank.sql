5430e62cf068e8dc4bb1758beeb0f8bedcde5b8d
M-nogales <manuelnogalesserrano5221@gmail.com>
2023-11-24 13:10:03 +0100

    modify bank ddbb

--- sql/Bank.sql
+++ sql/Bank.sql
@@ -2,13 +2,34 @@ DROP DATABASE IF EXISTS clear_bank;
 CREATE DATABASE clear_bank;
 use clear_bank;
 
+DROP DATABASE IF EXISTS clear_bank;
+CREATE DATABASE clear_bank;
+use clear_bank;
+
 -- añadir boolean numero rojos¿?
+CREATE TABLE Direcciones (
+    ID INT PRIMARY KEY AUTO_INCREMENT,
+    Provincia VARCHAR(50),
+    Cod_Postal VARCHAR(10),
+    Pais VARCHAR(10),
+    Ciudad VARCHAR(50),
+    Direccion VARCHAR(100)
+);
+
 CREATE TABLE Users (
     ID INT PRIMARY KEY AUTO_INCREMENT,
-    Nombre_user VARCHAR(50) NOT NULL,
-    Contraseña VARCHAR(50) NOT NULL,
+    Nombre VARCHAR(50) NOT NULL,
+    Apellidos VARCHAR(50),
+    DNI VARCHAR(9) UNIQUE,
+    Email VARCHAR(50) UNIQUE,
+    Pais VARCHAR(10),
+    Fecha_Nacimiento DATE,
+    Foto VARCHAR(255),
+    Clave VARCHAR(50) NOT NULL,
     Saldo DECIMAL(10, 2),
-    IBAN VARCHAR(30)
+    IBAN VARCHAR(30),
+    Direccion_ID INT,
+    FOREIGN KEY (Direccion_ID) REFERENCES Direcciones(ID)
 );
 
 CREATE TABLE Prestamos (
@@ -17,11 +38,12 @@ CREATE TABLE Prestamos (
     Cantidada_solicitada DECIMAL(10, 2),
     Mensualidad DECIMAL(10, 2),
     Motivo TEXT,
+    Aceptada boolean,
     FOREIGN KEY (User_ID) REFERENCES Users(ID)
 );
 
 CREATE TABLE Admins(
     ID INT PRIMARY KEY AUTO_INCREMENT,
     Nombre VARCHAR(50) NOT NULL,
-    Contraseña VARCHAR(50) NOT NULL
+    Contrasena VARCHAR(50) NOT NULL
 );
