c9875222cda3c541d89d4181396105be70a448d7
M-nogales <manuelnogalesserrano5221@gmail.com>
2023-12-05 10:50:10 +0100

    consultas basicas

--- php/ajustes.php
+++ php/ajustes.php
@@ -1,5 +1,39 @@
 <?php
+include_once('conex.php');
 // modificar ddbb, operaciones idem enviar, relación-tabla reflexiva
-function changeSettings(){
+function changeSettings($conn,$id_user,$fecha_nacimiento,$foto,$dirección,$codigoPostal,$ciudad,$provincia,$pais){
+    // Actualizar datos en la tabla Users
+    $updateUsers = "UPDATE Users 
+                    SET Fecha_Nacimiento = '$fecha_nacimiento', Foto = '$foto'
+                    WHERE ID = $id_user";
+
+    mysqli_query($conn, $updateUsers) or die("Error al actualizar datos en Users");
+
+    // Obtener el ID de la dirección actual del usuario
+    $selectDireccionID = "SELECT Direcciones_ID FROM Users WHERE ID = $id_user";
+    $resultDireccionID = mysqli_query($conn, $selectDireccionID) or die("Error al obtener ID de dirección");
+    $rowDireccionID = mysqli_fetch_assoc($resultDireccionID);
+    $direccionID = $rowDireccionID['Direcciones_ID'];
+
+    // Actualizar datos en la tabla Direcciones
+    $updateDirecciones = "UPDATE Direcciones 
+                          SET Pais = '$pais', Provincia = '$provincia', Cod_Postal = '$codigoPostal', Ciudad = '$ciudad'
+                          WHERE ID = $direccionID";
+
+    mysqli_query($conn, $updateDirecciones) or die("Error al actualizar datos en Direcciones");
+
+    echo "Datos actualizados con éxito.";
+    
 
 }
+//!sacar de funcion
+$id_user=1;
+$born_date = "1990-05-15";
+$foto = "ruta/de/la/foto2.jpg";
+$direccion = "Calle Principal 123";
+$CDP = "12345";
+$city = "Ciudad Principal";
+$provincia = "Provincia Principal";
+$pais = "Francia";
+
+changeSettings($conn,$id_user,$born_date,$foto,$direccion,$CDP,$city,$provincia,$pais);
